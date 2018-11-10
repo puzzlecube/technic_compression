9c3eff04caef7b0e92412e1c07f3b7c68e7129df
puzzlecube <drockmega@gmail.com>
2018-11-10 10:46:56 -0600

    Remove my own oversights of recent technic commits and add a couple recipes for consistancy.

--- init.lua
+++ init.lua
@@ -1,48 +1,19 @@
---[[local function override_recipe(input, output)
-	local recipe_table = technic.recipes.compressing.recipes
-	for index,recipe in pairs(recipe_table) do -- these recipes must be hijaked
-		dump(index)
-		dump(recipe)
-		dump(recipe_table)
-	end
-end]]
-
-
-
-
 local recipes = {
-	{"default:dirt 4",          "moreblocks:dirt_compressed"},
-	{"default:cobble 4",             "moreblocks:cobble_compressed"},
-	{"default:silver_sandstone", "default:stone"}, -- ?
-	{"default:desert_sandstone", "default:desert_stone"},
-	{"default:sandstone", "default:stone"},
--- overrides
-	{"default:desert_sand",        "default:desert_sandstone"},
-	{"default:silver_sand", "default:silver_sandstone"},
+	{"default:dirt 9",          "moreblocks:dirt_compressed"},
+	{"default:cobble 9",             "moreblocks:cobble_compressed"},
+	{"default:silver_sandstone", "default:stone 2"}, -- For consistancy: would change if compressing one desert sand didn't create one desert stone
+	{"default:desert_sandstone", "default:desert_stone 2"},
+	{"default:sandstone 2", "default:stone"},
+	{"default:silver_sand", "default:stone"},
+	--{"default:desert_sand",        "default:desert_sandstone"},
+	--{"default:silver_sand", "default:silver_sandstone"},
 }
 
 --local recipe_overrides = {}
 
 -- defuse the default recipes since we have the compressor to take over in a more realistic manner
-local desert = "default:desert_sand"
-local silver = "default:silver_sand"
 local cobble = "default:cobble"
 local dirt = "default:dirt"
-minetest.clear_craft({
-	output = desert.."stone",
-	recipe = {
-		{desert, desert},
-		{desert, desert}
-	},
-})
-
-minetest.clear_craft({
-	output = silver.."stone",
-	recipe = {
-		{silver, silver},
-		{silver, silver}
-	},
-})
 
 minetest.clear_craft({
 	output = "moreblocks:cobble_compressed",
@@ -65,7 +36,3 @@ minetest.clear_craft({
 for _, data in pairs(recipes) do
 	technic.register_compressor_recipe({input = {data[1]}, output = data[2]})
 end
-
---[[for _, data in pairs(recipe_overrides) do
-	override_recipe(data[1], data[2])
-end]]
